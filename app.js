dc9c3440f812c13fa513be2ad8a2ff9095f2ec61
tooler <1836783249@qq.com>
2020-11-17 13:50:15 +0800

    添加parser组件
    对小程序图片的点开等操作

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
 //app.js
 App({
   onLaunch: function () {
+    try {
+      const res = wx.getStorageInfoSync()
+     // console.log(res.keys)
+     // console.log(res.currentSize)
+     // console.log(res.limitSize)
+     // console.log(res.limitSize - 500)
+     // var guidKeys = res.keys.filter(o => o.length == 36);
+     // var guidKeys = res.keys.filter(o => this.isGuidFormat(o));
+
+      if (res.currentSize >= (res.limitSize - 500)) {
+        var guidKeys = res.keys.filter(o => this.isGuidFormat(o));
+        guidKeys.forEach(async (key) =>{
+          try {
+            await wx.removeStorageSync(key)
+          } catch (e) {
+            console.log(`清楚缓存【${key}]出错:${e}`)
+          }
+        });
+      }
+    } catch (e) {
+      console.log(e)
+    }
+  },
+  isGuidFormat: function (guid) {
+    var reg = new RegExp(/^[0-9a-zA-Z]{8}-[0-9a-zA-Z]{4}-[0-9a-zA-Z]{4}-[0-9a-zA-Z]{4}-[0-9a-zA-Z]{12}$/);
+    if (reg.test(guid)) {
+      return true;
+    }
+    return false;
   },
   globalData: {
   },
-  baseApi:"https://www.api.jzbl.com/api/"
+  baseApi: "https://www.api.jzbl.com/api/"
 })
--- pages/contentDetails/contentDetails.js
+++ pages/contentDetails/contentDetails.js
@@ -9,7 +9,7 @@ Page({
    */
   data: {
     detailsData: Object,
-    detailsHtml: "<p>这是些文字，没有什么用的文字很长很长。。什么用的文字很长很长。。。什么用的文字很长很长。。。什么用的文字很长很长。。。什么用的文字很长很长。。。。</p><p><img src=\"https://www.pic1.jzbl.com/itemfiles/3eb62c63-e608-41bf-95f4-30312481e96e/06bdf7ea-9b77-4b2d-aa63-2fd1b781b664/6367728977707766585595141.jpg?x-oss-process=image/resize,m_lfit,h_626,w_1000/quality,q_90/watermark,image_V2F0ZXJtYXJrL3dhdGVybWFyay5wbmc=,size_20,type_d3F5LXplbmhlaQ,text_QOmDqOiQveW7uuetkQ,color_E94743,shadow_50,order_0,align_2,interval_10,t_90,g_sw,x_20,y_20\" width=\"100%\" title=\"3ece2f94-fd6a-4a21-9a7b-414aefabe657.jpg\"/></p><p><img src=\"https://www.pic1.jzbl.com/itemfiles/3eb62c63-e608-41bf-95f4-30312481e96e/06bdf7ea-9b77-4b2d-aa63-2fd1b781b664/6367728977707766585595141.jpg?x-oss-process=image/resize,m_lfit,h_626,w_1000/quality,q_90/watermark,image_V2F0ZXJtYXJrL3dhdGVybWFyay5wbmc=,size_20,type_d3F5LXplbmhlaQ,text_QOmDqOiQveW7uuetkQ,color_E94743,shadow_50,order_0,align_2,interval_10,t_90,g_sw,x_20,y_20\" width=\"100%\" title=\"3ece2f94-fd6a-4a21-9a7b-414aefabe657.jpg\"/></p><p><img src=\"https://www.pic1.jzbl.com/itemfiles/3eb62c63-e608-41bf-95f4-30312481e96e/06bdf7ea-9b77-4b2d-aa63-2fd1b781b664/6367728977707766585595141.jpg?x-oss-process=image/resize,m_lfit,h_626,w_1000/quality,q_90/watermark,image_V2F0ZXJtYXJrL3dhdGVybWFyay5wbmc=,size_20,type_d3F5LXplbmhlaQ,text_QOmDqOiQveW7uuetkQ,color_E94743,shadow_50,order_0,align_2,interval_10,t_90,g_sw,x_20,y_20\" width=\"100%\" title=\"3ece2f94-fd6a-4a21-9a7b-414aefabe657.jpg\"/></p><p><img src=\"https://www.pic1.jzbl.com/itemfiles/3eb62c63-e608-41bf-95f4-30312481e96e/06bdf7ea-9b77-4b2d-aa63-2fd1b781b664/6367728977707766585595141.jpg?x-oss-process=image/resize,m_lfit,h_626,w_1000/quality,q_90/watermark,image_V2F0ZXJtYXJrL3dhdGVybWFyay5wbmc=,size_20,type_d3F5LXplbmhlaQ,text_QOmDqOiQveW7uuetkQ,color_E94743,shadow_50,order_0,align_2,interval_10,t_90,g_sw,x_20,y_20\" width=\"100%\" title=\"3ece2f94-fd6a-4a21-9a7b-414aefabe657.jpg\"/></p><p><img src=\"https://www.pic1.jzbl.com/itemfiles/3eb62c63-e608-41bf-95f4-30312481e96e/06bdf7ea-9b77-4b2d-aa63-2fd1b781b664/6367728977707766585595141.jpg?x-oss-process=image/resize,m_lfit,h_626,w_1000/quality,q_90/watermark,image_V2F0ZXJtYXJrL3dhdGVybWFyay5wbmc=,size_20,type_d3F5LXplbmhlaQ,text_QOmDqOiQveW7uuetkQ,color_E94743,shadow_50,order_0,align_2,interval_10,t_90,g_sw,x_20,y_20\" width=\"100%\" title=\"3ece2f94-fd6a-4a21-9a7b-414aefabe657.jpg\"/></p>"
+   // detailsHtml:''
   },
 
   /**
@@ -25,13 +25,13 @@ Page({
       })
       return;
     }
-    // wx.showLoading({
-    //   title: '加载中',
-    // })
+    wx.showLoading({
+      title: '加载中',
+    })
     setTimeout(() => {
       this.getDetails(id);
 
-     // wx.hideLoading()
+     wx.hideLoading()
     }, 1000);
 
   },
@@ -91,6 +91,7 @@ Page({
       this.setData({ detailsData: details });
       wx.setNavigationBarTitle({
         title: this.data.detailsData.ItemEntity.ItemName,
+       // detailsHtml:this.detailsHtml(this.data.detailsData.ItemEntity.ItemContentBefore)
       })
     } else {
       var detailsUrl = baseUrl + `ItemDetails/Details`;
@@ -99,15 +100,32 @@ Page({
       util.request_method_post_data(detailsUrl, para, (res) => {
         if (res.Success) {
           var datas = res.Data;
-          console.log(datas)
+          if(datas.ItemEntity.ItemContentBefore!=''){
+            datas.ItemEntity.ItemContentBefore=this.detailsHtml(datas.ItemEntity.ItemContentBefore);
+          }
+          //console.log(datas)
           wx.setStorageSync(id, datas);
           this.setData({ detailsData: datas });
 
           wx.setNavigationBarTitle({
             title: this.data.detailsData.ItemEntity.ItemName,
+            //detailsHtml:this.detailsHtml(this.data.detailsData.ItemEntity.ItemContentBefore)
           })
         }
       });
     }
+  },
+  detailsHtml:function(stringObj){
+   // console.log("原数据："+stringObj)
+    if(stringObj.length!=0){
+      stringObj = stringObj.replace(/src=\"\/content\/images\/loading.gif"/g,' ');
+      stringObj = stringObj.replace(/data-original/g,'src');
+      stringObj = stringObj.replace(/width=\"855\"/g,'');
+      stringObj = stringObj.replace(/<img/g,'<img width=\"100%\"')
+    }
+
+   // console.log("后面数据："+stringObj);
+    return stringObj;
   }
+
 })
